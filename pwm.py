a0c493a48d3b3148d665423a88c68b09fbd92e93
Tom Øyvind Hogstad <tom.oyvind.hogstad@vtfk.no>
2019-10-22 11:32:05 +0200

    LA til pwm

---
+++ pwm.py
@@ -0,0 +1,11 @@
+from machine import Pin, PWM
+
+led_1 = PWM(Pin(25)) # Setter opp PWM på Pinne med standard verdier
+led_1.freq() # Returnerer frekvensen på pwm 
+led_1.duty()
+led_1.freq(4000) # Setter frekvensen til 4000Hz
+led_1.duty(512) # Setter duty til 512 av 1024 - 50%
+
+for i in range(512): # Øker styrken fra 0 til 50%
+    led_1.duty(i) 
+    time.sleep(0.2) # Øker intensitetetn sakte 
